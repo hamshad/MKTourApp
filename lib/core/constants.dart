d7407b02ba4a29ee3287d84cb426e0eaaf0c460b
hamshad <hiromega18@gmail.com>
2026-09-11 18:22:59 +0530

    feat: isDev flag

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -1,4 +1,6 @@
+import 'constants/api_constants.dart';
+
 class AppConstants {
-  // Use 10.0.2.2 for Android Emulator, localhost for iOS Simulator
-  static const String apiBaseUrl = 'http://localhost:3000/api';
+  static const bool isDev =
+      ApiConstants.socketUrl != 'https://api.mktours.co.uk';
 }
